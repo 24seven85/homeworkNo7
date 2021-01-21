27f6bbe47ba515974114fdbd200ffd3c9c9fbf38
maksimkjaulakis <24seven@live.ru>
2021-01-22 01:43:33 +0200

    try-catch

--- homeworkNo7/main.swift
+++ homeworkNo7/main.swift
@@ -10,11 +10,10 @@ import Foundation
 enum CardError: Error {
     case outOfMoney
     case overCardLimit
+    case lessMinCashout
 }
 enum TerminalError: Error {
     case outOfBalance
-    case overMaxCashOut
-    case lessMinCashout
     case overMaxDeposit
 }
 extension CardError: LocalizedError {
@@ -22,6 +21,7 @@ extension CardError: LocalizedError {
         switch self {
         case .outOfMoney: return "Не хватает средств на карте"
         case .overCardLimit: return "Превышен лимит для снятия"
+        case .lessMinCashout: return "Слишком маленькая сумма для снятия"
         }
     }
 }
@@ -31,54 +31,65 @@ extension TerminalError: LocalizedError {
         
         switch self {
         case .outOfBalance: return "В банкомате недостаточно средств"
-        case .overMaxCashOut: return "Превышен лимит снятия банкомата"
-        case .lessMinCashout: return "Слишком маленькая сумма"
         case .overMaxDeposit: return "Слишком большая сумма вклада"
         }
     }
 }
 
 
-struct Card {
+class Card {
     var balance: Double = 2000
     let cashOutLimit: Double = 300
-    
+    let cashoutMin: Double = 5
 }
 class Terminal {
-    var termBalance = 10000
-    let maxCashOut = 5000
-    let minCashOut = 10
-    let maxDeposit = 1500
+    var termBalance: Double = 10000
+    let maxDeposit: Double = 1500
 
 
     func cashOut( amount: Double) -> CardError? {
         var card = Card()
+        
         guard card.balance > amount else {
             return .outOfMoney
         }
         guard card.cashOutLimit >= amount else {
             return .overCardLimit
         }
+        guard card.cashoutMin <= amount else {
+            return .lessMinCashout
+        }
         
         
         card.balance -= amount
+        terminal.termBalance -= amount
         print("Со счета снято \(amount) $")
         return nil
     }
     
-    func cashIn() {
-        
+    func cashIn(amount: Double) throws {
+        var card = Card()
+        var terminal = Terminal()
+        guard terminal.maxDeposit >= amount else {
+            throw TerminalError.overMaxDeposit
+        }
+        card.balance += amount
+        print("Баланс счета увеличился на \(amount) $")
     }
 }
 
 
-
 let terminal = Terminal()
 
-if let error = terminal.cashOut(amount: 300) {
+if let error = terminal.cashOut(amount: 5) {
     print(error.localizedDescription)
-   
+
 }
 
 
+do {
+    try terminal.cashIn(amount: 1600)
+} catch let error {
+    print(error.localizedDescription)
+}
 
